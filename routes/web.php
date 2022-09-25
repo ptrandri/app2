1192c50230ed24431a5f53af05bedafb0e2715af
ptrandri <putrayanto528@gmail.com>
2022-09-25 21:21:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About',
+        "name" => 'Andri Putra',
+        "email" => 'ptrandri@hotmail.com'
+    ]);
+});
+
+Route::get('/blogs', function () {
+    return view('posts', [
+        'title' => 'Posts'
+    ]);
+});
+
+Route::get('/login', [LoginController::class, 'index']);
