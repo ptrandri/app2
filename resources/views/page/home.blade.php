d75fd6b85fc3ae5cefaaaaf8057986899d573d12
ptrandri <putrayanto528@gmail.com>
2022-10-09 02:34:31 +0700

    change layout home, add image home

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,4 +1,27 @@
 @extends('layout.main')
 @section('container')
-    <h1> Halaman Home </h1>
+    <section id="home" class="home">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-8 pt-4 pt-lg-0 content">
+                    <h2 class="fw-bold">Ticket Application</h2>
+                    <h3>Deliver faster support with an easy-to-use TicketAPP Application</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, necessitatibus blanditiis. Sequi ea
+                        deleniti quaerat officiis voluptatem ab vitae, enim obcaecati distinctio! Excepturi dolorum earum id
+                        natus maxime sequi officia.
+                    </p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, reprehenderit magnam tenetur optio
+                        quae doloremque vero accusantium autem repellat laborum rem, esse asperiores nobis molestiae error
+                        quaerat, quia doloribus ducimus.
+                    </p>
+                    <a href="/register" type="button" class="btn btn-primary">Let's Start</a>
+                </div>
+                <div class="col">
+                    <img src="/assets/images/app.gif" width="400" class="img-fluid" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
