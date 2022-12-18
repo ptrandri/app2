d337c7e85f83e004ace4af6e9747699c4e2d3236
ptrandri <putrayanto528@gmail.com>
2022-12-19 00:40:36 +0700

    add livewire laravel new

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,16 +1,19 @@
 @extends('dashboard.layouts.main')
+@push('styles')
+    @livewireStyles
+@endpush
+
+@push('scripts')
+    @livewireScripts()
+@endpush
+
 @section('container')
     <div class="container-fluid">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
             <h1 class="h5">Ticket List</h1>
             <a href="/tickets/create">
-                <button type="button" class="btn btn-primary">Add Ticket</button>
+                <button type="button" class="btn btn-primary mb-2">Add Ticket</button>
             </a>
-        </div>
-
-        <div
-            class="form-outline d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-1 pb-2 mb-3 border-bottom search">
-            <input type="search" name="search" id="search" placeholder="Search something" class="form-control">
         </div>
 
         <div class="table-responsive">
@@ -20,43 +23,8 @@
                     <strong>{{ session('message') }}</strong>
                 </div>
             @endif
-            <table id="tickets" class="table table-striped table-sm">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Agent Name</th>
-                    <th scope="col">Subject Case</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Caller Name</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Priority</th>
-                    <th scope="col">Assigned To</th>
-                    <th scope="col">Created Date</th>
-                    <th scope="col">Updated Date</th>
-                </tr>
-                <tbody class="alldata">
-                    @foreach ($tickets as $ticket)
-                        <tr>
-                            <td>{{ $ticket->id }}</td>
-                            <td>{{ $ticket->AgentName }}</td>
-                            <td><a href="/tickets/{{ $ticket->id }}">{{ $ticket->SubjectCase }}</a></td>
-                            <td>{{ $ticket->SubjectDesc }}</td>
-                            <td>{{ $ticket->CallerName }}</td>
-                            <td>{{ $ticket->Status }}</td>
-                            <td>{{ $ticket->Priority }}</td>
-                            <td>{{ $ticket->Assigned_to }}</td>
-                            <td>{{ $ticket->created_at }}</td>
-                            <td>{{ $ticket->updated_at }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-                <tbody id="Content" class="searchdata"> </tbody>
-            </table>
-            {{-- Pagination --}}
-            <div class="d-flex justify-content-end m-1">
-                {!! $tickets->links() !!}
-            </div>
-            {{-- end Pagination --}}
-
+            @livewire('search-pagination')
         </div>
+
     </div>
 @endsection
